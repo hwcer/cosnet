0e221eb601b6708b1f08795a19a6c5f16eb7fb38
hwcer <hwclegend@163.com>
2023-03-24 20:30:58 +0800

    -

--- socket.go
+++ socket.go
@@ -230,7 +230,6 @@ func (this *Socket) writeMsgTrue(msg *Message) (r bool) {
 		this.Errorf(err)
 		return true
 	}
-	//logger.Debug("socket write error,code:%v, data:%v", msg.Code(), msg.data())
 	var n int
 	writeCount := 0
 	for writeCount < len(data) {
